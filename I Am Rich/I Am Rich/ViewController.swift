9b09aa77eae242c5f9295196bb5bdd3ef96c10a9
Devesh Avutu <devesh14may@gmail.com>
2021-10-04 11:36:37 +0530

    uploaded more projects

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Devesh Avutu on 03/07/20.
+//  Copyright © 2020 Devesh Avutu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
